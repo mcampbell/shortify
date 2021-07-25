c143783c401a7412a8148007aecc7f2b6eba7762
Michael Campbell <michael.campbell@byzzer.com>
2021-07-25 11:30:44 -0400

    cleanup

--- src/bin/cleanup-docker.sh
+++ src/bin/cleanup-docker.sh
@@ -3,7 +3,7 @@
 docker stop shortify_{client,server} 2>/dev/null
 docker rm shortify_{client,server} 2>/dev/null
 
-( docker images | egrep "shortify_(client|server)" | awk '{print $3}' ) | xargs -r docker rmi
+( docker images | egrep "shortify.(client|server)" | awk '{print $3}' ) | xargs -r docker rmi
 
 # If you have a docker-cleanup.sh in ~/bin, run it.
 [ -x "$HOME/bin/docker-cleanup.sh" ] && "$HOME/bin/docker-cleanup.sh"
--- src/client/bin/docker.run.sh
+++ src/client/bin/docker.run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 docker run -it --rm \
+       --name shortify-client \
        -d \
        -p 5000:5000 \
        shortify-client:latest
